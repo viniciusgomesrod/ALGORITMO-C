b830cbfdde30fbde1001f3352ef18ead7050af2e
Vinicius Gomes <viniciusgomes2202@gmail.com>
2024-05-27 11:40:02 -0300

    Create contar_elemento_array.c

---
+++ contar_elemento_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int contarElemento(int array[], int elemento, int tamanho){
+	if(tamanho == 0){
+		return 0;
+	}
+	int ocorrencias = contarElemento(array, elemento, tamanho - 1);
+	if(array[tamanho-1] == elemento){
+		ocorrencias++;
+	}
+	return ocorrencias;
+}
+
+int main(void) {
+	int arr[] = {1, 2, 4, 4, 4};
+	int tamanho = sizeof(arr) / sizeof(arr[0]);
+	//printf("Tamanho: %d", tamanho);
+	int resultado = contarElemento(arr, 4, tamanho);
+	printf("Quantidade de aparições do elemento: %d\n", resultado);
+	return 0;
+}
