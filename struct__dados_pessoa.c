ccc7f0195cdd1bb7c39424037f4956eab3a11ce9
Vinicius Gomes <viniciusgomes2202@gmail.com>
2024-06-10 10:57:24 -0300

    Create struct__dados_pessoa.c

---
+++ struct__dados_pessoa.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float Peso;
+    int Idade;
+    float Altura;
+} Pessoa;
+
+Pessoa SetPessoa(int idade, float peso, float altura) {
+    Pessoa P;
+    P.Idade = idade;
+    P.Peso = peso;
+    P.Altura = altura;
+    return P;
+}
+
+void ImprimePessoa(Pessoa P) {
+    printf("Idade: %d, anos  Peso: %.2f kgs, Altura: %.2f\n, metro.", P.Idade, P.Peso, P.Altura);
+}
+
+int main(void) {
+    Pessoa PessoaDigitada;
+
+    printf("Digite a idade da pessoa: ");
+    scanf("%d", &PessoaDigitada.Idade);
+
+    printf("Digite o peso da pessoa: ");
+    scanf("%f", &PessoaDigitada.Peso);
+
+    printf("Digite a altura da pessoa: ");
+    scanf("%f", &PessoaDigitada.Altura);
+
+    ImprimePessoa(PessoaDigitada);
+
+    return 0;
+}
